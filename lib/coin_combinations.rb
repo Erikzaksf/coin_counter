f6b92d695dfa0af453b24d9b27e2f95fe24fa59f
erik zakrzewski and linda luu <linda@>
2017-09-07 13:34:16 -0700

    add working method to meet last two specs

--- lib/coin_combinations.rb
+++ lib/coin_combinations.rb
@@ -2,6 +2,8 @@
 public def coin_counter(cents)
     quarters = 0
     dimes = 0
+    nickels = 0
+    pennies = 0
     cents = cents.to_i
 
     if cents >= 25
@@ -11,9 +13,15 @@ public def coin_counter(cents)
     elsif cents >= 10
       dimes = cents / 10
       cents = cents % 10
-    else
 
+    elsif cents >= 5
+      nickels = cents / 5
+      cents = cents % 5
+
+    elsif cents >= 1
+      pennies = cents / 1
+      cents = cents % 1
   end
-  array = [quarters, dimes]
+  array = [quarters, dimes, nickels, pennies]
 end
 #puts "hi how are you today"
--- spec/coin_combinations_spec.rb
+++ spec/coin_combinations_spec.rb
@@ -2,10 +2,16 @@ require ('rspec')
 require ('coin_combinations')
 
 describe('#coin_counter') do
-  it ("return the least amount of cents") do
-    expect(coin_counter(50)).to(eq([2, 0]))
+  it ("return the least amount of cents quarters") do
+    expect(coin_counter(50)).to(eq([2, 0, 0, 0]))
   end
   it ("return the least amount of cents for dimes") do
-    expect(coin_counter(10)).to(eq([0,1]))
+    expect(coin_counter(10)).to(eq([0,1, 0, 0]))
+  end
+  it ("return the least amount of cents for nickels") do
+    expect(coin_counter(5)).to(eq([0,0,1,0]))
+  end
+  it ("return the least amount of cents for pennies") do
+    expect(coin_counter(1)).to(eq([0,0,0,1]))
   end
 end
